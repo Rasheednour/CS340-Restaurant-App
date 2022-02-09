15f852dac646991f66bb2c5cde72ff0b0274c74b
Rasheednour <rasheednour@gmail.com>
2022-02-10 02:55:04 +0300

    Added generic content to home page

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Restaurant Home Page<h1>"
+    return render_template("index.html")
 
 @app.route("/create-account")
 def user():
