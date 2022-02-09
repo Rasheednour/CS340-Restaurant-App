9841b81fda0cf32add9a3ca8e33b151dffc5b5f2
Rasheednour <rasheednour@gmail.com>
2022-02-09 07:32:41 +0300

    initial commit

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask 
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "<h1>Restaurant Home Page<h1>"
+
+@app.route("/create-account")
+def user():
+    return "<h1>Create new account<h1>"
+
+@app.route("/order")
+def order():
+    return "<h1>Order Food<h1>"
+
+@app.route("/payment")
+def payment():
+    return "<h1>Pay<h1>"
+
+@app.route("/addresses")
+def addresses():
+    return "<h1>Address List<h1>"
+
+if __name__ == "__main__":
+    app.run()
