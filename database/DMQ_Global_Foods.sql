2c32f3c17422dfdc6c75cae27c767a9a4ab72ce8
Rasheednour <rasheednour@gmail.com>
2022-03-09 02:57:47 +0300

    Updated insertions in DMQ

--- database/DMQ_Global_Foods.sql
+++ database/DMQ_Global_Foods.sql
@@ -1,8 +1,8 @@
 
 -- Customers
 -- create 
-INSERT INTO `Customers` VALUES (:ID, :web_supplied_AddrID, :web_supplied_fname, :web_supplied_lname, :web_supplied_email, :web_supplied_phone);
-
+INSERT INTO Customers (customerID, currentAddress, firstName, lastName, email, phoneNumber)
+VALUES (:ID, :web_supplied_AddrID, :web_supplied_fname, :web_supplied_lname, :web_supplied_email, :web_supplied_phone);
 -- delete
 DELETE FROM Customers WHERE id = :web_supplied_CustomerID OR fname = :web_supplied_fname;
 
@@ -16,8 +16,8 @@ SELECT * from Customers WHERE lname = :web_supplied_lname;
 -- ===========================
 -- Orders
 -- create
-INSERT INTO `Orders` VALUES    (;ID, :web_supplied_customerID, ;web_supplied_progress , :web_supplied_price, :web_supplied_date);
-
+INSERT INTO Orders (orderID, customerID, orderProgress, totalPrice, orderDate)
+VALUES    (;ID, :web_supplied_customerID, ;web_supplied_progress , :web_supplied_price, :web_supplied_date);
 -- delete
 DELETE FROM Orders WHERE id = :web_supplied_OrderID;
 
@@ -31,8 +31,8 @@ SELECT * from Orders WHERE customerID = :web_supplied_customerID;
 -- ===========================
 -- OrderItems
 -- create
-INSERT INTO `OrderItems` VALUES    (:web_supplied_OrderID, :web_supplied_foodID, :web_supplied_quantity, :web_supplied_totalPrice);
-
+INSERT INTO OrderItems (orderID, foodID, quantity, totalPrice)
+VALUES    (:web_supplied_OrderID, :web_supplied_foodID, :web_supplied_quantity, :web_supplied_totalPrice);
 -- delete
 DELETE FROM OrderItems WHERE id = :web_supplied_OrderItemID;
 
@@ -45,7 +45,9 @@ SELECT * FROM OrderItems WHERE orderID = :web_supplied_orderID;
 -- ===========================
 -- Foods
 -- create
-INSERT INTO `Foods` VALUES     (, :web_supplied_foodName, :web_supplied_foodDesc, :web_supplied_foodCategory, :web_supplied_foodCalory, :web_supplied_cuisine, :web_supplied_price);
+INSERT INTO Foods (foodID, foodName, foodDescription, foodCategory, calories, cuisine, price)
+VALUES (:web_supplied_foodID, :web_supplied_foodName, :web_supplied_foodDesc, :web_supplied_foodCategory, :web_supplied_foodCalory, :web_supplied_cuisine, :web_supplied_price);
+
 
 -- delete
 DELETE FROM Foods WHERE id = :web_supplied_foodID;
@@ -58,7 +60,7 @@ SELECT * FROM Foods WHERE foodName = :web_supplied_foodName;
 -- ===========================
 -- Payments
 -- create
-INSERT INTO `Payments` (`paymentID`, `customerID`, `orderID`, `paymentDate`, `paymentAmount`, `paymentMethod`)
+INSERT INTO Payments (paymentID, customerID, orderID, paymentDate, paymentAmount, paymentMethod)
 VALUES  (:web_supplied_paymentID, :web_supplied_customerID, :web_supplied_orderID, :web_supplied_date, :web_supplied_amount, :web_supplied_payment);
 
 -- delete
@@ -73,7 +75,8 @@ SELECT * FROM Payments WHERE paymentId= :web_supplied_paymentId;
 -- ===========================
 -- Addresses
 -- create
-INSERT INTO `Addresses` VALUES (:web_supplied_addressId, :web_supplied_city, :web_supplied_streetName, :web_supplied_streetNumber);
+INSERT INTO Addresses (addressID, city, streetName, streetNumber)
+VALUES (:web_supplied_addressId, :web_supplied_city, :web_supplied_streetName, :web_supplied_streetNumber);
 
 -- delete
 DELETE FROM Addresses WHERE addressId = :web_supplied_ID;
