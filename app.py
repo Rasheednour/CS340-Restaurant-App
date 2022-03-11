337dccdaeb1baf0f82cd523a5ddc621391de40df
Rasheednour <rasheednour@gmail.com>
2022-03-11 05:50:09 +0300

    Implemented DELETE accross all tables

--- app.py
+++ app.py
@@ -43,21 +43,57 @@ def restaurant_database():
 
         if 'delete-food' in data:
             values = (data["foodID"],)
-            sql = ('''DELETE FROM orderItems WHERE foodID = (%s)''')
+            sql = ('''DELETE FROM OrderItems WHERE foodID = (%s)''')
             cur.execute(sql, values)
-            sql = ('''DELETE FROM foods WHERE foodID = (%s)''')
+            sql = ('''DELETE FROM Foods WHERE foodID = (%s)''')
             cur.execute(sql, values)
             mysql.connection.commit()
 
-        if 'food-form' in data:
+        elif 'delete-order' in data:
+            values = (data["orderID"],)
+            sql = ('''DELETE FROM OrderItems WHERE orderID = (%s)''')
+            cur.execute(sql, values)
+            sql = ('''DELETE FROM Payments WHERE orderID = (%s)''')
+            cur.execute(sql, values)
+            sql = ('''DELETE FROM Orders WHERE orderID = (%s)''')
+            cur.execute(sql, values)
+            mysql.connection.commit()
+
+        elif 'delete-item' in data:
+            values = (data["orderID"],)
+            sql = ('''DELETE FROM OrderItems WHERE orderID = (%s)''')
+            cur.execute(sql, values)
+            mysql.connection.commit()
+
+        elif 'delete-payment' in data:
+            values = (data["paymentID"],)
+            sql = ('''DELETE FROM Payments WHERE paymentID = (%s)''')
+            cur.execute(sql, values)
+            mysql.connection.commit()
+
+        elif 'delete-customer' in data:
+            values = (data["customerID"],)
+            sql = ('''DELETE FROM Addresses WHERE customerID = (%s)''')
+            cur.execute(sql, values)
+            sql = ('''DELETE FROM Customers WHERE customerID = (%s)''')
+            cur.execute(sql, values)
+            mysql.connection.commit()
+
+        elif 'delete-address' in data:
+            values = (data["addressID"],)
+            sql = ('''DELETE FROM Addresses WHERE addressID = (%s)''')
+            cur.execute(sql, values)
+            mysql.connection.commit()
+
+        elif 'food-form' in data:
             values = (data["name"], data["description"], data["category"], data["calories"], data["cuisine"], data["price"])
-            sql = ('''INSERT INTO foods (foodName, foodDescription, foodCategory, calories, cuisine, price)
+            sql = ('''INSERT INTO Foods (foodName, foodDescription, foodCategory, calories, cuisine, price)
                     VALUES (%s, %s, %s, %s, %s, %s)''')
             cur.execute(sql, values)
             mysql.connection.commit()
 
         elif 'search-form' in data:
-            sql = (''' SELECT * FROM foods WHERE foodName = (%s) ''')
+            sql = (''' SELECT * FROM Foods WHERE foodName = (%s) ''')
             search = data["search"]
             cur.execute(sql, [search])
             search_result = cur.fetchall()
@@ -65,7 +101,7 @@ def restaurant_database():
 
         elif 'order-form' in data:
             values = (data["customerID"], data["orderProgress"], data["totalPrice"], data["orderDate"])
-            sql = ('''INSERT INTO orders (customerID, orderProgress, totalPrice, orderDate)
+            sql = ('''INSERT INTO Orders (customerID, orderProgress, totalPrice, orderDate)
                     VALUES (%s, %s, %s, %s)''')
             cur.execute(sql, values)
             mysql.connection.commit()
@@ -73,7 +109,7 @@ def restaurant_database():
 
         elif 'items-form' in data:
             values = (data["orderID"], data["foodID"], data["quantity"], data["totalPrice"])
-            sql = ('''INSERT INTO orderitems (orderID, foodID, quantity, totalPrice)
+            sql = ('''INSERT INTO OrderItems (orderID, foodID, quantity, totalPrice)
                     VALUES (%s, %s, %s, %s)''')
             cur.execute(sql, values)
             mysql.connection.commit()
@@ -81,7 +117,7 @@ def restaurant_database():
 
         elif 'payments-form' in data:
             values = (data["customerID"], data["orderID"], data["paymentDate"], data["paymentAmount"], data["paymentMethod"])
-            sql = ('''INSERT INTO payments (customerID, orderID, paymentDate, paymentAmount, paymentMethod)
+            sql = ('''INSERT INTO Payments (customerID, orderID, paymentDate, paymentAmount, paymentMethod)
                     VALUES (%s, %s, %s, %s, %s)''')
             cur.execute(sql, values)
             mysql.connection.commit()
@@ -89,7 +125,7 @@ def restaurant_database():
 
         elif 'customers-form' in data:
             values = (data["firstName"], data["lastName"], data["email"], data["phoneNumber"])
-            sql = ('''INSERT INTO customers (firstName, lastName, email, phoneNumber)
+            sql = ('''INSERT INTO Customers (firstName, lastName, email, phoneNumber)
                     VALUES (%s, %s, %s, %s)''')
             cur.execute(sql, values)
             mysql.connection.commit()
@@ -97,94 +133,35 @@ def restaurant_database():
 
         elif 'addresses-form' in data:
             values = (data["city"], data["streetName"], data["streetNumber"])
-            sql = ('''INSERT INTO addresses (city, streetName, streetNumber)
+            sql = ('''INSERT INTO Addresses (city, streetName, streetNumber)
                     VALUES (%s, %s, %s)''')
             cur.execute(sql, values)
             mysql.connection.commit()
             link = "/pages/addresses.html"
 
-    print("food search is", food_search)
     cur = mysql.connection.cursor()
 
-    cur.execute('''SELECT * from foods''')
+    cur.execute('''SELECT * from Foods''')
     foods = cur.fetchall()
-    cur.execute('''SELECT * from orders''')
+    cur.execute('''SELECT * from Orders''')
     orders = cur.fetchall()
 
-    cur.execute('''SELECT * from orderItems''')
+    cur.execute('''SELECT * from OrderItems''')
     order_items = cur.fetchall()
 
-    cur.execute('''SELECT * from customers''')
+    cur.execute('''SELECT * from Customers''')
     customers = cur.fetchall()
 
-    cur.execute('''SELECT * from addresses''')
+    cur.execute('''SELECT * from Addresses''')
     addresses = cur.fetchall()
 
-    cur.execute('''SELECT * from payments''')
+    cur.execute('''SELECT * from Payments''')
     payments = cur.fetchall()
 
     return render_template(link, foods=foods, orders=orders, 
                                      order_items=order_items, customers=customers, 
                                            addresses=addresses, payments=payments, 
                                      food_search=food_search)
-
-
-@app.route("/restauarant-database", methods=['DELETE'])
-def delete_row():
-    
-    cur = mysql.connection.cursor()
-    data = request.form
-    print("did i get here?")
-
-    if 'delete-food' in data:
-        values = (data["delete-food"])
-        print("values are: ", values)
-        # sql = ('''DELETE FROM foods WHERE foodID = (%s)''')
-        # cur.execute(sql, values)
-        # mysql.connection.commit()
-        restaurant_database()
-
-    elif 'delete-order' in data:
-        values = (data["customerID"], data["orderProgress"], data["totalPrice"], data["orderDate"])
-        sql = ('''DELETE FROM orders (customerID, orderProgress, totalPrice, orderDate)
-                VALUES (%s, %s, %s, %s)''')
-        cur.execute(sql, values)
-        mysql.connection.commit()
-        restaurant_database()
-
-    elif 'delete-item' in data:
-        values = (data["orderID"], data["foodID"], data["quantity"], data["totalPrice"])
-        sql = ('''DELETE FROM orderitems (orderID, foodID, quantity, totalPrice)
-                VALUES (%s, %s, %s, %s)''')
-        cur.execute(sql, values)
-        mysql.connection.commit()
-        restaurant_database()
-
-    elif 'delete-payment' in data:
-        values = (data["customerID"], data["orderID"], data["paymentDate"], data["paymentAmount"], data["paymentMethod"])
-        sql = ('''DELETE FROM payments (customerID, orderID, paymentDate, paymentAmount, paymentMethod)
-                VALUES (%s, %s, %s, %s, %s)''')
-        cur.execute(sql, values)
-        mysql.connection.commit()
-        restaurant_database()
-
-    elif 'delete-customer' in data:
-        values = (data["firstName"], data["lastName"], data["email"], data["phoneNumber"])
-        sql = ('''DELETE FROM customers (firstName, lastName, email, phoneNumber)
-                VALUES (%s, %s, %s, %s)''')
-        cur.execute(sql, values)
-        mysql.connection.commit()
-        restaurant_database()
-
-    elif 'delete-address' in data:
-        values = (data["city"], data["streetName"], data["streetNumber"])
-        sql = ('''DELETE FROM addresses (city, streetName, streetNumber)
-                VALUES (%s, %s, %s)''')
-        cur.execute(sql, values)
-        mysql.connection.commit()
-        restaurant_database()
-
-
 
 
 
--- database/DDQ_Global_Foods.sql
+++ database/DDQ_Global_Foods.sql
@@ -4,14 +4,14 @@
 -- Table structure for table `Foods`
 --
 
-DROP TABLE IF EXISTS `payments`;
-DROP TABLE IF EXISTS `orderItems`;
-DROP TABLE IF EXISTS `orders`;
-DROP TABLE IF EXISTS `customers`;
-DROP TABLE IF EXISTS `addresses`;
-DROP TABLE IF EXISTS `foods`;
+DROP TABLE IF EXISTS `Payments`;
+DROP TABLE IF EXISTS `OrderItems`;
+DROP TABLE IF EXISTS `Orders`;
+DROP TABLE IF EXISTS `Customers`;
+DROP TABLE IF EXISTS `Addresses`;
+DROP TABLE IF EXISTS `Foods`;
 
-CREATE TABLE `foods` (
+CREATE TABLE `Foods` (
   `foodID`          int NOT NULL AUTO_INCREMENT,
   `foodName`        varchar(50) NOT NULL,
   `foodDescription` varchar(255) NOT NULL,
@@ -26,110 +26,114 @@ CREATE TABLE `foods` (
 -- Dumping data for table `Foods`
 --
 
-INSERT INTO `foods` VALUES     (1, 'Mangolian Beef', 'Stir fryed sliced flank steak with soy sauce and green onions', 'Main Dishes', 800, 'Chinese', 20.50),
+INSERT INTO `Foods` VALUES     (1, 'Mangolian Beef', 'Stir fryed sliced flank steak with soy sauce and green onions', 'Main Dishes', 800, 'Chinese', 20.50),
                                (2, 'Sugarcane Juice', 'Delicious juice made from sugarcanes imported from Thaliand', 'Drinks', 150, 'Thai', 4.50),
                                (3, 'Zurek', 'Fermented soup made with sour rye flour', 'Appetizers', 120, 'Polish', 8.50);
 
 
---
--- Table structure for table `Addresses`
---
 
-DROP TABLE IF EXISTS `addresses`;
-
-CREATE TABLE `addresses` (
-  `addressID`       int NOT NULL AUTO_INCREMENT,
-  `city`            varchar(50) NOT NULL,
-  `streetName`      varchar(50) NOT NULL,
-  `streetNumber`    int NOT NULL,
-  PRIMARY KEY (`addressID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
-
---
--- Dumping data for table `Addresses`
---
-
-INSERT INTO `addresses` VALUES (1, 'Boston', 'Raven Street', 25345),
-                               (2, 'Dallas', '6th Avenue', 235523),
-                               (3, 'Sao Paolo', 'Church Road', 55643);
 
 
 --
 -- Table structure for table `Customers`
 --
 
-DROP TABLE IF EXISTS `customers`;
+DROP TABLE IF EXISTS `Customers`;
 
-CREATE TABLE `customers` (
+CREATE TABLE `Customers` (
   `customerID`      int NOT NULL AUTO_INCREMENT,
   `currentAddress`  int DEFAULT NULL,
   `firstName`       varchar(50) NOT NULL,
   `lastName`        varchar(50) NOT NULL,
   `email`           varchar(70) NOT NULL,
-  `phoneNumber`     int NOT NULL,
-  PRIMARY KEY (`customerID`),
-  KEY `currentAddress` (`currentAddress`),
-  FOREIGN KEY (`currentAddress`) REFERENCES `addresses` (`addressID`)
+  `phoneNumber`     varchar(50) NOT NULL,
+  PRIMARY KEY (`customerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `Customers`
 --
 
-INSERT INTO `customers` VALUES (1, 1, 'Alfred', 'Clark', 'alf.clark@hotmail.com', 021244812),
-                               (2, 2, 'Lisa', 'Young', 'lisa-1992k@gmail.com', 021349553),
-                               (3, 3, 'Ali', 'Omer', 'ali-omer@yahoo.com', 021140012);
+INSERT INTO `Customers` VALUES (1, NULL, 'Alfred', 'Clark', 'alf.clark@hotmail.com', '02-1244-812'),
+                               (2, NULL, 'Lisa', 'Young', 'lisa-1992k@gmail.com', '02-134-9553'),
+                               (3, NULL, 'Ali', 'Omer', 'ali-omer@yahoo.com', '02-114-0012');
 
+
+--
+-- Table structure for table `Addresses`
+--
+
+DROP TABLE IF EXISTS `Addresses`;
+
+CREATE TABLE `Addresses` (
+  `addressID`       int NOT NULL AUTO_INCREMENT,
+  `customerID`      int NOT NULL,
+  `city`            varchar(50) NOT NULL,
+  `streetName`      varchar(50) NOT NULL,
+  `streetNumber`    int NOT NULL,
+  PRIMARY KEY (`addressID`),
+  KEY `customerID` (`customerID`),
+  FOREIGN KEY (`customerID`) REFERENCES `Customers` (`customerID`)
+
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+--
+-- Dumping data for table `Addresses`
+--
+
+INSERT INTO `Addresses` VALUES (1, 1, 'Boston', 'Raven Street', 25345),
+                               (2, 2, 'Dallas', '6th Avenue', 235523),
+                               (3, 3, 'Sao Paolo', 'Church Road', 55643);
 
 
 --
 -- Table structure for table `Orders`
 --
 
-DROP TABLE IF EXISTS `orders`;
+DROP TABLE IF EXISTS `Orders`;
 
-CREATE TABLE `orders` (
+CREATE TABLE `Orders` (
   `orderID`         int NOT NULL AUTO_INCREMENT,
   `customerID`      int NOT NULL,
   `orderProgress`   varchar(50) NOT NULL,
-  `totalPrice`      float NOT NULL,
+  `totalPrice`      float DEFAULT NULL,
   `orderDate`       date NOT NULL,
   PRIMARY KEY (`orderID`),
   KEY `customerID` (`customerID`),
-  FOREIGN KEY (`customerID`) REFERENCES `customers` (`customerID`)
+  FOREIGN KEY (`customerID`) REFERENCES `Customers` (`customerID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `Orders`
 --
 
-INSERT INTO `orders` VALUES    (1, 1, 'New', 41.00, STR_TO_DATE('02/12/2021', '%m/%d/%Y')),
-                               (2, 2, 'Preparing', 13.50, STR_TO_DATE('02/09/2021', '%m/%d/%Y')),
-                               (3, 3, 'Delivered', 25.50, STR_TO_DATE('01/23/2021', '%m/%d/%Y'));
+INSERT INTO `Orders` VALUES    (1, 1, 'New', NULL, STR_TO_DATE('02/12/2021', '%m/%d/%Y')),
+                               (2, 2, 'New', NULL, STR_TO_DATE('02/09/2021', '%m/%d/%Y')),
+                               (3, 3, 'New', NULL, STR_TO_DATE('01/23/2021', '%m/%d/%Y'));
 
 
 --
 -- Table structure for table `OrderItems`
 --
 
-DROP TABLE IF EXISTS `orderItems`;
+DROP TABLE IF EXISTS `OrderItems`;
 
-CREATE TABLE `orderItems` (
+CREATE TABLE `OrderItems` (
   `orderID`         int NOT NULL,
   `foodID`          int NOT NULL,
   `quantity`        int NOT NULL,
   `totalPrice`      float NOT NULL,
   KEY `orderID` (`orderID`),
   KEY `foodID` (`foodID`),
-  FOREIGN KEY (`orderID`) REFERENCES `orders` (`orderID`),
-  FOREIGN KEY (`foodID`) REFERENCES `foods` (`foodID`)
+  FOREIGN KEY (`orderID`) REFERENCES `Orders` (`orderID`),
+  FOREIGN KEY (`foodID`) REFERENCES `Foods` (`foodID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `OrderItems`
 --
 
-INSERT INTO `orderItems` VALUES    (1, 1, 2, 41.00),
+INSERT INTO `OrderItems` VALUES    (1, 1, 2, 41.00),
                                (2, 2, 3, 13.50),
                                (3, 3, 3, 25.50);
 
@@ -138,9 +142,9 @@ INSERT INTO `orderItems` VALUES    (1, 1, 2, 41.00),
 -- Table structure for table `Payments`
 --
 
-DROP TABLE IF EXISTS `payments`;
+DROP TABLE IF EXISTS `Payments`;
 
-CREATE TABLE `payments` (
+CREATE TABLE `Payments` (
   `paymentID`      int NOT NULL AUTO_INCREMENT,
   `customerID`     int NOT NULL,
   `orderID`        int NOT NULL,
@@ -150,15 +154,15 @@ CREATE TABLE `payments` (
   PRIMARY KEY (`paymentID`),
   KEY `customerID` (`customerID`),
   KEY `orderID` (`orderID`),
-  FOREIGN KEY (`customerID`) REFERENCES `customers` (`customerID`),
-  FOREIGN KEY (`orderID`) REFERENCES `orders` (`orderID`)
+  FOREIGN KEY (`customerID`) REFERENCES `Customers` (`customerID`),
+  FOREIGN KEY (`orderID`) REFERENCES `Orders` (`orderID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
 --
 -- Dumping data for table `Payments`
 --
 
-INSERT INTO `payments` (`paymentID`, `customerID`, `orderID`, `paymentDate`, `paymentAmount`, `paymentMethod`)
+INSERT INTO `Payments` (`paymentID`, `customerID`, `orderID`, `paymentDate`, `paymentAmount`, `paymentMethod`)
 VALUES  (1, 1, 1, STR_TO_DATE('02/12/2021', '%m/%d/%Y'), 41.00, 'Credit Card'),
         (2, 2, 2, STR_TO_DATE('02/09/2021', '%m/%d/%Y'), 13.50, 'Credit Card'),
         (3, 3, 3, STR_TO_DATE('01/23/2021', '%m/%d/%Y'), 25.50, 'Cash');
