c0edfce2c27a2ac25456c3536ab3b6105e4699e4
Abdullah <76535164+adabz@users.noreply.github.com>
2022-03-11 05:04:44 +0300

    tested delete db function successfully, needs to have 3 arguments passed: table(string), type of id(string), id(number)

--- app.py
+++ app.py
@@ -24,165 +24,139 @@ mysql = MySQL(app)
 @app.route("/")
 def home():
 
-    # cur = mysql.connection.cursor()
-    # cur.execute('''DROP TABLE IF EXISTS example''')
+    # db_instance= mysql.connection.cursor()
+    # db_instance.execute('''DROP TABLE IF EXISTS example''')
 
     # return "done"
     return render_template("home.html")
 
 
 # listing the restrant database
+# this function needs to be broken into two functions, first a function that reads arguments and pass them to second function
+# second function then performs actions shown in the bottom in if() statements.
 @app.route("/restauarant-database", methods=['POST', 'GET'])
-def restaurant_database():
+def process_request():
     food_search = ()
     link = "restaurant-database.html"
 
     if request.method == 'POST':
-        cur = mysql.connection.cursor()
+        db_instance = mysql.connection.cursor()
         data = request.form
-
+        
         # check if it's a search post method
-        if 'food-form' in data:
+        if 'insert-food-form' in data:
             values = (data["name"], data["description"], data["category"], data["calories"], data["cuisine"], data["price"])
-            sql = ('''INSERT INTO foods (foodName, foodDescription, foodCategory, calories, cuisine, price)
+            query = ('''INSERT INTO foods (foodName, foodDescription, foodCategory, calories, cuisine, price)
                     VALUES (%s, %s, %s, %s, %s, %s)''')
-            cur.execute(sql, values)
+            db_instance.execute(query, values)
             mysql.connection.commit()
 
         elif 'search-form' in data:
-            sql = (''' SELECT * FROM foods WHERE foodName = (%s) ''')
+            query= (''' SELECT * FROM foods WHERE foodName = (%s) ''')
             search = data["search"]
-            cur.execute(sql, [search])
-            search_result = cur.fetchall()
+            db_instance.execute(sql, [search])
+            search_result = db_instance.fetchall()
             food_search = search_result
 
-        elif 'order-form' in data:
+        elif 'insert-order-form' in data:
             values = (data["customerID"], data["orderProgress"], data["totalPrice"], data["orderDate"])
-            sql = ('''INSERT INTO orders (customerID, orderProgress, totalPrice, orderDate)
+            query= ('''INSERT INTO orders (customerID, orderProgress, totalPrice, orderDate)
                     VALUES (%s, %s, %s, %s)''')
-            cur.execute(sql, values)
+            db_instance.execute(sql, values)
             mysql.connection.commit()
             link = "/pages/orders.html"
 
-        elif 'items-form' in data:
+        elif 'insert-items-form' in data:
             values = (data["orderID"], data["foodID"], data["quantity"], data["totalPrice"])
-            sql = ('''INSERT INTO orderitems (orderID, foodID, quantity, totalPrice)
+            query= ('''INSERT INTO orderitems (orderID, foodID, quantity, totalPrice)
                     VALUES (%s, %s, %s, %s)''')
-            cur.execute(sql, values)
+            db_instance.execute(sql, values)
             mysql.connection.commit()
             link = "/pages/items.html"
 
-        elif 'payments-form' in data:
+        elif 'insert-payments-form' in data:
             values = (data["customerID"], data["orderID"], data["paymentDate"], data["paymentAmount"], data["paymentMethod"])
-            sql = ('''INSERT INTO payments (customerID, orderID, paymentDate, paymentAmount, paymentMethod)
+            query= ('''INSERT INTO payments (customerID, orderID, paymentDate, paymentAmount, paymentMethod)
                     VALUES (%s, %s, %s, %s, %s)''')
-            cur.execute(sql, values)
+            db_instance.execute(sql, values)
             mysql.connection.commit()
             link = "/pages/payments.html"
 
-        elif 'customers-form' in data:
+        elif 'insert-customers-form' in data:
             values = (data["firstName"], data["lastName"], data["email"], data["phoneNumber"])
-            sql = ('''INSERT INTO customers (firstName, lastName, email, phoneNumber)
+            query= ('''INSERT INTO customers (firstName, lastName, email, phoneNumber)
                     VALUES (%s, %s, %s, %s)''')
-            cur.execute(sql, values)
+            db_instance.execute(sql, values)
             mysql.connection.commit()
             link = "/pages/customers.html"
 
-        elif 'addresses-form' in data:
+        elif 'insert-addresses-form' in data:
             values = (data["city"], data["streetName"], data["streetNumber"])
-            sql = ('''INSERT INTO addresses (city, streetName, streetNumber)
+            query= ('''INSERT INTO addresses (city, streetName, streetNumber)
                     VALUES (%s, %s, %s)''')
-            cur.execute(sql, values)
+            db_instance.execute(sql, values)
             mysql.connection.commit()
             link = "/pages/addresses.html"
 
-    print("food search is", food_search)
-    cur = mysql.connection.cursor()
+        elif 'delete' in data:
+            return rem_item(request.form)
 
-    cur.execute('''SELECT * from foods''')
-    foods = cur.fetchall()
-    cur.execute('''SELECT * from orders''')
-    orders = cur.fetchall()
+    
+    return get_items()
 
-    cur.execute('''SELECT * from orderItems''')
-    order_items = cur.fetchall()
+# get database items
+def get_items():
+    food_search = ()
+    link = "restaurant-database.html"
 
-    cur.execute('''SELECT * from customers''')
-    customers = cur.fetchall()
+    db_instance= mysql.connection.cursor()
 
-    cur.execute('''SELECT * from addresses''')
-    addresses = cur.fetchall()
+    db_instance.execute('''SELECT * from foods''')
+    foods = db_instance.fetchall()
+    db_instance.execute('''SELECT * from orders''')
+    orders = db_instance.fetchall()
 
-    cur.execute('''SELECT * from payments''')
-    payments = cur.fetchall()
+    db_instance.execute('''SELECT * from orderItems''')
+    order_items = db_instance.fetchall()
+
+    db_instance.execute('''SELECT * from customers''')
+    customers = db_instance.fetchall()
+
+    db_instance.execute('''SELECT * from addresses''')
+    addresses = db_instance.fetchall()
+
+    db_instance.execute('''SELECT * from payments''')
+    payments = db_instance.fetchall()
 
     return render_template(link, foods=foods, orders=orders, 
-                                     order_items=order_items, customers=customers, 
-                                           addresses=addresses, payments=payments, 
-                                     food_search=food_search)
+                          order_items=order_items, customers=customers, 
+                          addresses=addresses, payments=payments, 
+                          food_search=food_search)
 
-# # processing POST request on 'resturant database' page
-# @app.route("/restauarant-database", methods=['POST'])
-# def database_post():
-#     cur = mysql.connection.cursor()
-#     data = request.form
 
-#     # check if it's a search post method
-#     if 'food-form' in data:
-#         values = (data["name"], data["description"], data["category"], data["calories"], data["cuisine"], data["price"])
-#         sql = ('''INSERT INTO foods (foodName, foodDescription, foodCategory, calories, cuisine, price)
-#                 VALUES (%s, %s, %s, %s, %s, %s)''')
-#         cur.execute(sql, values)
-#         mysql.connection.commit()
-#         return restaurant_database()
+# remove an item 
+@app.route("/restauarant-database", methods=['POST'])
+def rem_item(param):
 
-#     elif 'search-form' in data:
-#         sql = (''' SELECT * FROM foods WHERE foodName = (%s) ''')
-#         search = data["search"]
-#         cur.execute(sql, [search])
-#         search_result = cur.fetchall()
-#         food_search = search_result
-#         return restaurant_database()
+    table = param['table']   # type of table to delete from
+    idType = param['idType'] # type of id to delete
+    delID = param[idType]    # actual id to delete
+    
 
-#     elif 'order-form' in data:
-#         values = (data["customerID"], data["orderProgress"], data["totalPrice"], data["orderDate"])
-#         sql = ('''INSERT INTO orders (customerID, orderProgress, totalPrice, orderDate)
-#                 VALUES (%s, %s, %s, %s)''')
-#         cur.execute(sql, values)
-#         mysql.connection.commit()
-#         return restaurant_database()
+    enab = "SET FOREIGN_KEY_CHECKS=0"
 
-#     elif 'items-form' in data:
-#         values = (data["orderID"], data["foodID"], data["quantity"], data["totalPrice"])
-#         sql = ('''INSERT INTO orderitems (orderID, foodID, quantity, totalPrice)
-#                 VALUES (%s, %s, %s, %s)''')
-#         cur.execute(sql, values)
-#         mysql.connection.commit()
-#         return restaurant_database()
+    print(param)
 
-#     elif 'payments-form' in data:
-#         values = (data["customerID"], data["orderID"], data["paymentDate"], data["paymentAmount"], data["paymentMethod"])
-#         sql = ('''INSERT INTO payments (customerID, orderID, paymentDate, paymentAmount, paymentMethod)
-#                 VALUES (%s, %s, %s, %s, %s)''')
-#         cur.execute(sql, values)
-#         mysql.connection.commit()
-#         return restaurant_database()
+    query = "DELETE FROM " + table + " WHERE " + idType + " = " + delID
+    
+    db_instance= mysql.connection.cursor()
+    
+    db_instance.execute(enab)
+    db_instance.execute(query)
 
-#     elif 'customers-form' in data:
-#         values = (data["firstName"], data["lastName"], data["email"], data["phoneNumber"])
-#         sql = ('''INSERT INTO customers (firstName, lastName, email, phoneNumber)
-#                 VALUES (%s, %s, %s, %s)''')
-#         cur.execute(sql, values)
-#         mysql.connection.commit()
-#         return restaurant_database()
+    mysql.connection.commit()
 
-#     elif 'addresses-form' in data:
-#         values = (data["city"], data["streetName"], data["streetNumber"])
-#         sql = ('''INSERT INTO addresses (city, streetName, streetNumber)
-#                 VALUES (%s, %s, %s)''')
-#         cur.execute(sql, values)
-#         mysql.connection.commit()
-#         return restaurant_database()
+    return get_items() 
 
 if __name__ == "__main__":
 
